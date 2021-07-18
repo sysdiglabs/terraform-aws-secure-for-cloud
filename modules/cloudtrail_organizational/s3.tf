e9b6702392946f52aea0d4bec93a8a01e643c499
iru <irune.prado@sysdig.com>
2021-07-18 22:47:05 +0200

    fix: S3 policy and public access conflict bug leading to 409 responses

--- modules/cloudtrail_organizational/s3.tf
+++ modules/cloudtrail_organizational/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_public_access_block" "cloudtrail" {
   bucket              = aws_s3_bucket.cloudtrail.id
   block_public_acls   = true
   block_public_policy = true
-  //  depends_on = [aws_s3_bucket.cloudtrail] // required for destroy order force
+  depends_on          = [aws_s3_bucket_policy.cloudtrail_s3] # https://github.com/hashicorp/terraform-provider-aws/issues/7628
 }
 
 
