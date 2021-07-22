13020e1e79b40739714b159315312f11f76fe1e0
iru <irune.prado@sysdig.com>
2021-07-22 20:08:34 +0200

    feat: non-public s3-config

--- modules/services/s3.tf
+++ modules/services/s3.tf
@@ -4,11 +4,25 @@ resource "aws_s3_bucket" "s3_config_bucket" {
   bucket        = "${var.name}-${data.aws_caller_identity.me.account_id}-config"
   acl           = "private"
   force_destroy = true
-
   versioning {
     enabled = true
   }
   tags = var.tags
+}
+
+
+# --------------------------
+# iam, acl
+# -------------------------
+
+resource "aws_s3_bucket_public_access_block" "s3_config_bucket" {
+  bucket = aws_s3_bucket.s3_config_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
 }
 
 resource "aws_s3_bucket_policy" "allow_cloudvision_role" {
