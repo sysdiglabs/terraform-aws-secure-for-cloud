deefcd05994fc867eaea69fcf663c7c58917be68
iru <irune.prado@sysdig.com>
2021-07-25 16:24:13 +0200

    feat: enable vpc to service resources
    chore: add acl condition vpc source, on cloudtrail resources (s3,sns)

--- main.tf
+++ main.tf
@@ -10,13 +10,24 @@ provider "aws" {
 # cloudvision submodules
 #-------------------------------------
 
+module "services_vpc" {
+  source = "./modules/services_vpc"
+  providers = {
+    aws = aws.cloudvision
+  }
+}
+
+
 module "cloudtrail_organizational" {
   source = "./modules/cloudtrail_organizational"
 
   cloudvision_account_id = var.org_cloudvision_account_id
   is_multi_region_trail  = var.cloudtrail_org_is_multi_region_trail
   s3_kms_enable          = var.cloudtrail_org_s3_kms_enable
-  tags                   = var.tags
+
+  cloudvision_vpc_id = module.services_vpc.vpc_id
+
+  tags = var.tags
 }
 
 
@@ -30,5 +41,10 @@ module "services" {
   sysdig_secure_api_token  = var.sysdig_secure_api_token
   cloudtrail_sns_topic_arn = module.cloudtrail_organizational.sns_topic_arn
   services_assume_role_arn = aws_iam_role.cloudvision_role.arn
-  tags                     = var.tags
+
+  services_vpc_id              = module.services_vpc.vpc_id
+  services_vpc_private_subnets = module.services_vpc.vpc_private_subnets
+  services_sg_id               = module.services_vpc.vpc_sg_id
+
+  tags = var.tags
 }
