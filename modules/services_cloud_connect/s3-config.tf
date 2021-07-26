b2ea2df369656ab821f48af8eb9a103b977a9326
iru <irune.prado@sysdig.com>
2021-07-26 14:36:49 +0200

    chore: remove security-hub

--- modules/services_cloud_connect/s3-config.tf
+++ modules/services_cloud_connect/s3-config.tf
@@ -6,17 +6,13 @@ resource "aws_s3_bucket_object" "config" {
   bucket  = data.aws_s3_bucket.config.id
   key     = "cloud-connector.yaml"
   content = local.default_config
-  // content = (var.config_content == null && var.config_source == null) ? local.default_config : var.config_content
-  //  source  = var.config_source # TODO content or source, not both
-  tags = var.tags
+  tags    = var.tags
 }
 
 locals {
   default_config = <<CONFIG
 logging: info
 rules:
-  - secure:
-      url: ""
   - s3:
       bucket: ${var.config_bucket}
       path: rules
@@ -29,10 +25,6 @@ notifiers:
   - cloudwatch:
       logGroup: ${aws_cloudwatch_log_group.log.name}
       logStream: ${aws_cloudwatch_log_stream.stream.name}
-  - securityhub:
-      productArn: arn:aws:securityhub:${data.aws_region.current.name}::product/sysdig/sysdig-cloud-connector
-  - secure:
-      url: ""
 CONFIG
 
   task_env_vars = concat([
