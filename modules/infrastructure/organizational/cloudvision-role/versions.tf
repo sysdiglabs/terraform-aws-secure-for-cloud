c5c438396b43d3dc0ebbe0396c3f93c6e31aca0a
iru <irune.prado@sysdig.com>
2021-08-03 17:00:35 +0200

    chore: bump terraform version on role module

--- modules/infrastructure/organizational/cloudvision-role/versions.tf
+++ modules/infrastructure/organizational/cloudvision-role/versions.tf
@@ -1,8 +1,7 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.15.0"
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
       version               = ">= 3.50.0"
       configuration_aliases = [aws.member]
     }
