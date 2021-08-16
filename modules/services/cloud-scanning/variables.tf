9031ea4fcdc3ed9afad701ff87824de4f9ba7fe6
iru <wideawakening@protonmail.com>
2021-08-16 13:19:37 +0200

    Update variables.tf

--- modules/services/cloud-scanning/variables.tf
+++ modules/services/cloud-scanning/variables.tf
@@ -1,6 +1,11 @@
+
 #---------------------------------
-# cloud-scanning parametrization
+# cloud-scanning specific
 #---------------------------------
+variable "secure_api_token_secret_name" {
+  type        = string
+  description = "Sysdig Secure API Token secret name"
+}
 
 variable "build_project_arn" {
   type        = string
@@ -37,27 +42,15 @@ variable "vpc_subnets" {
   description = "Subnets where the CloudScanning will be deployed"
 }
 
-variable "secure_api_token_secret_name" {
-  type        = string
-  description = "Sysdig Secure API Token secret name"
-}
 
 
 #---------------------------------
 # optionals - with default
 #---------------------------------
 
-variable "sysdig_secure_endpoint" {
-  type        = string
-  default     = "https://secure.sysdig.com"
-  description = "Sysdig Secure API endpoint"
-}
-
-variable "name" {
-  type        = string
-  default     = "cloud-scanning"
-  description = "Name for the Cloud Scanning deployment"
-}
+# 
+# cloud-scanning specific
+#
 
 variable "image" {
   type = string
@@ -72,22 +65,38 @@ variable "cloudwatch_log_retention" {
   description = "Days to keep logs for CloudScanning"
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "sysdig cloudvision tags"
-  default = {
-    "product" = "sysdig-cloudvision"
-  }
-}
-
 variable "verify_ssl" {
   type        = bool
   default     = true
-  description = "true/false to determine ssl verification"
+  description = "true/false to determine ssl secure connection verification"
 }
 
 variable "extra_env_vars" {
   type        = map(string)
   default     = {}
   description = "Extra environment variables for the Cloud Scanning deployment"
+}
+
+
+#
+# misc
+#
+variable "sysdig_secure_endpoint" {
+  type        = string
+  default     = "https://secure.sysdig.com"
+  description = "Sysdig Secure API endpoint"
+}
+
+variable "name" {
+  type        = string
+  default     = "cloud-scanning"
+  description = "Name for the Cloud Scanning deployment"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "sysdig cloudvision tags"
+  default = {
+    "product" = "sysdig-cloudvision"
+  }
 }
