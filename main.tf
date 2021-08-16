5125c2f6d3083cc33b09744c77eec431bfb069a6
iru <irune.prado@sysdig.com>
2021-08-16 14:16:25 +0200

    chore: disable scanning for the moment as won't work for organizational

--- main.tf
+++ main.tf
@@ -16,15 +16,6 @@ module "ssm" {
   sysdig_secure_api_token = var.sysdig_secure_api_token
 }
 
-# FIXME? if this is a non-shared resource, move its usage to scanning service
-module "codebuild" {
-  source                       = "./modules/infrastructure/codebuild"
-  name                         = var.name
-  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
-  depends_on                   = [module.ssm]
-}
-
-
 module "cloudtrail" {
   source = "./modules/infrastructure/cloudtrail"
   name   = var.name
@@ -87,26 +78,36 @@ module "cloud_connector" {
 
 
 
-module "cloud_scanning" {
-  providers = {
-    aws = aws.cloudvision
-  }
+## FIXME? if this is a non-shared resource, move its usage to scanning service?
+#module "codebuild" {
+#  source                       = "./modules/infrastructure/codebuild"
+#  name                         = var.name
+#  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
+#  depends_on                   = [module.ssm]
+#}
+#
 
-  source = "./modules/services/cloud-scanning"
-  name   = "${var.name}-cloudscanning"
 
-  sysdig_secure_endpoint       = var.sysdig_secure_endpoint
-  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
-
-  build_project_arn  = module.codebuild.project_arn
-  build_project_name = module.codebuild.project_name
-
-  sns_topic_arn = module.cloudtrail.sns_topic_arn
-
-  ecs_cluster = module.ecs_fargate_cluster.id
-  vpc_id      = module.ecs_fargate_cluster.vpc_id
-  vpc_subnets = module.ecs_fargate_cluster.vpc_subnets
-
-  tags       = var.tags
-  depends_on = [module.cloudtrail, module.ecs_fargate_cluster, module.codebuild, module.ssm]
-}
+#module "cloud_scanning" {
+#  providers = {
+#    aws = aws.cloudvision
+#  }
+#
+#  source = "./modules/services/cloud-scanning"
+#  name   = "${var.name}-cloudscanning"
+#
+#  sysdig_secure_endpoint       = var.sysdig_secure_endpoint
+#  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
+#
+#  build_project_arn  = module.codebuild.project_arn
+#  build_project_name = module.codebuild.project_name
+#
+#  sns_topic_arn = module.cloudtrail.sns_topic_arn
+#
+#  ecs_cluster = module.ecs_fargate_cluster.id
+#  vpc_id      = module.ecs_fargate_cluster.vpc_id
+#  vpc_subnets = module.ecs_fargate_cluster.vpc_subnets
+#
+#  tags       = var.tags
+#  depends_on = [module.cloudtrail, module.ecs_fargate_cluster, module.codebuild, module.ssm]
+#}
