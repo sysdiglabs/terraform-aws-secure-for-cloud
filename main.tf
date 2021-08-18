8ae0f824b174609457fb19aaf1091f33b7ea34f3
iru <irune.prado@sysdig.com>
2021-08-18 12:42:45 +0200

    chore: provide verify_ssl options

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+# FIXME. refact verify_ssl so its handled in upper layers and passed downwards
+locals {
+  verify_ssl = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false: true
+}
+
 provider "sysdig" {
-  sysdig_secure_url       = var.sysdig_secure_endpoint
-  sysdig_secure_api_token = var.sysdig_secure_api_token
+  sysdig_secure_url           = var.sysdig_secure_endpoint
+  sysdig_secure_api_token     = var.sysdig_secure_api_token
+  sysdig_secure_insecure_tls  = local.verify_ssl
 }
 
 #-------------------------------------
@@ -82,8 +88,11 @@ module "cloud_connector" {
   depends_on = [module.cloudtrail, module.ecs_fargate_cluster, module.ssm]
 }
 
-data "aws_caller_identity" "me" {}
 
+
+
+
+data "aws_caller_identity" "me" {}
 module "cloud_bench" {
   providers = {
     aws = aws.cloudvision
@@ -93,6 +102,8 @@ module "cloud_bench" {
   account_id = var.is_organizational ? var.organizational_config.cloudvision_member_account_id : data.aws_caller_identity.me.account_id
   tags       = var.tags
 }
+
+
 
 
 ## FIXME? if this is a non-shared resource, move its usage to scanning service?
