f27c439d792f6279b8bccba845cac4cebba5c247
iru <irune.prado@sysdig.com>
2021-08-19 16:41:42 +0200

    chore(example): add single-account-scanning

---
+++ examples/single-account-scanning/variables.tf
@@ -0,0 +1,58 @@
+variable "sysdig_secure_api_token" {
+  sensitive   = true
+  type        = string
+  description = "Sysdig Secure API token"
+}
+
+
+#---------------------------------
+# optionals - with defaults
+#---------------------------------
+
+#
+# cloudtrail configuration
+#
+
+variable "cloudtrail_is_multi_region_trail" {
+  type        = bool
+  default     = true
+  description = "testing/economization purpose. true/false whether cloudtrail will ingest multiregional events"
+}
+
+variable "cloudtrail_kms_enable" {
+  type        = bool
+  default     = true
+  description = "testing/economization purpose. true/false whether s3 should be encrypted"
+}
+
+
+#
+# general
+#
+
+variable "name" {
+  type        = string
+  description = "Name for the Cloud Vision deployment"
+  default     = "sysdig-cloudvision"
+}
+
+
+variable "region" {
+  type        = string
+  default     = "eu-central-1"
+  description = "Default region for resource creation in both organization master and cloudvision member account"
+}
+
+variable "sysdig_secure_endpoint" {
+  type        = string
+  default     = "https://secure.sysdig.com"
+  description = "Sysdig Secure API endpoint"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "sysdig cloudvision tags"
+  default = {
+    "product" = "sysdig-cloudvision"
+  }
+}
