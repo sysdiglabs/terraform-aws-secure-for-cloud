99d4c4130d303182f8178777e2e15974cdaa3f49
iru <irune.prado@sysdig.com>
2021-08-19 10:34:12 +0200

    chore(cleanup): benchmark

--- examples/single-account-benchmark/main.tf
+++ examples/single-account-benchmark/main.tf
@@ -2,15 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-# FIXME. refact verify_ssl so its handled in upper layers and passed downwards
-locals {
-  verify_ssl = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false : true
-}
-
 provider "sysdig" {
   sysdig_secure_url          = var.sysdig_secure_endpoint
   sysdig_secure_api_token    = var.sysdig_secure_api_token
-  sysdig_secure_insecure_tls = local.verify_ssl
+  sysdig_secure_insecure_tls = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false : true
 }
 
 
