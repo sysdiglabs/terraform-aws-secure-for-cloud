53c4d020f5069b07e0975ffedc5dbdf5cc59f3d1
Noah Kraemer <noah.kraemer@sysdig.com>
2021-08-20 16:04:36 -0700

    cleanup var file

--- modules/services/cloud-bench/variables.tf
+++ modules/services/cloud-bench/variables.tf
@@ -1,5 +1,5 @@
 variable "account_id" {
-  type = string
+  type        = string
   description = "the account_id in which to provision the cloud-bench IAM role"
 }
 
@@ -8,7 +8,9 @@ variable "account_id" {
 #---------------------------------
 
 variable "regions" {
-  type = list(string)
+  type        = list(string)
+  description = "List of regions in which to run the benchmark"
+
   default = [
     "af-south-1",
     "eu-north-1",
@@ -32,15 +34,13 @@ variable "regions" {
     "us-west-1",
     "us-west-2",
   ]
-  description = "List of regions in which to run the benchmark"
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
   description = "sysdig secure-for-cloud tags"
+
   default = {
     "product" = "sysdig-secure-for-cloud"
   }
 }
-
-  "af-south-1",<br>  "eu-north-1",<br>  "ap-south-1",<br>  "eu-west-3",<br>  "eu-west-2",<br>  "eu-south-1",<br>  "eu-west-1",<br>  "ap-northeast-3",<br>  "ap-northeast-2",<br>  "me-south-1",<br>  "ap-northeast-1",<br>  "sa-east-1",<br>  "ca-central-1",<br>  "ap-east-1",<br>  "ap-southeast-1",<br>  "ap-southeast-2",<br>  "eu-central-1",<br>  "us-east-1",<br>  "us-east-2",<br>  "us-west-1",<br>  "us-west-2",<br>
