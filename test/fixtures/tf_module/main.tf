48d4bc248a02adc6b71d254071a8a017279a11c3
Hayk Kocharyan <hayk@MacBook-Pro-de-Hayk-Work.local>
2021-08-25 10:52:22 +0200

    fix: fix precommit

--- test/fixtures/tf_module/main.tf
+++ test/fixtures/tf_module/main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
 module "cloudvision_aws_single_account" {
   source = "../../../examples/single-account"
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
   name                    = var.name
-  region                  = "eu-west-3"
+  region                  = var.region
 }
