6432da991a02853f7765bbcc12512dcc08caa613
Néstor Salceda <nestor.salceda@sysdig.com>
2021-09-01 15:49:09 +0200

    feat: Add scanning deployment

--- examples/single-account-k8s/main.tf
+++ examples/single-account-k8s/main.tf
@@ -13,6 +13,12 @@ module "resource_group_master" {
   tags   = var.tags
 }
 
+module "ssm" {
+  source                  = "../../modules/infrastructure/ssm"
+  name                    = var.name
+  sysdig_secure_api_token = var.sysdig_secure_api_token
+}
+
 module "cloudtrail" {
   source                = "../../modules/infrastructure/cloudtrail"
   name                  = var.name
@@ -116,34 +122,93 @@ CONFIG
 # cloud-scanning
 #-------------------------------------
 
-#module "codebuild" {
-#  source                       = "../../modules/infrastructure/codebuild"
-#  name                         = var.name
-#  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
-#
-#  tags = var.tags
-#  # note. this is required to avoid racing conditions
-#  depends_on = [module.ssm]
-#}
-#
-#
-#module "cloud_scanning" {
-#  source = "../../modules/services/cloud-scanning"
-#  name   = "${var.name}-cloudscanning"
-#
-#  sysdig_secure_endpoint       = var.sysdig_secure_endpoint
-#  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
-#
-#  build_project_arn  = module.codebuild.project_arn
-#  build_project_name = module.codebuild.project_name
-#
-#  sns_topic_arn = module.cloudtrail.sns_topic_arn
-#
-#  ecs_cluster = module.ecs_fargate_cluster.id
-#  vpc_id      = module.ecs_fargate_cluster.vpc_id
-#  vpc_subnets = module.ecs_fargate_cluster.vpc_subnets
-#
-#  tags = var.tags
-#  # note. this is required to avoid racing conditions
-#  depends_on = [module.cloudtrail, module.ecs_fargate_cluster, module.codebuild, module.ssm]
-#}
+module "codebuild" {
+  source                       = "../../modules/infrastructure/codebuild"
+  name                         = var.name
+  secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
+
+  tags = var.tags
+  # note. this is required to avoid race conditions
+  depends_on = [module.ssm]
+}
+
+resource "aws_sqs_queue" "sqs_scanning" {
+  name = "${var.name}-scanning"
+  tags = var.tags
+}
+
+resource "aws_sns_topic_subscription" "sns_scanning" {
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.sqs_scanning.arn
+  topic_arn = module.cloudtrail.sns_topic_arn
+}
+
+resource "aws_sqs_queue_policy" "cloudtrail_sns_scanning" {
+  queue_url = aws_sqs_queue.sqs_scanning.id
+  policy    = data.aws_iam_policy_document.cloudtrail_sns_scanning.json
+
+  # required to avoid  error reading SQS Queue Policy; empty result
+  depends_on = [aws_sqs_queue.sqs_scanning]
+}
+
+data "aws_iam_policy_document" "cloudtrail_sns_scanning" {
+  statement {
+    sid    = "Allow CloudTrail to send messages"
+    effect = "Allow"
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "sqs:SendMessage",
+      "sqs:SendMessageBatch"
+    ]
+    resources = [aws_sqs_queue.sqs_scanning.arn]
+  }
+}
+
+resource "helm_release" "cloud_scanning" {
+  name       = "cloud-scanning"
+
+  #repository = "https://charts.sysdig.com"
+  #chart      = "cloud-scanning"
+  chart = "/home/nestor/Projects/work/sysdig/sysdiglabs/charts/charts/cloud-scanning"
+
+  create_namespace = true
+  namespace = var.name
+
+  set_sensitive {
+    name  = "sysdig.secureAPIToken"
+    value = var.sysdig_secure_api_token
+  }
+
+  set_sensitive {
+    name  = "aws.accessKeyId"
+    value = var.aws_access_key_id
+  }
+
+  set_sensitive {
+    name  = "aws.secretAccessKey"
+    value = var.aws_secret_access_key
+  }
+
+  set {
+    name  = "aws.region"
+    value = var.region
+  }
+
+  set {
+    name  = "sysdig.url"
+    value = var.sysdig_secure_endpoint
+  }
+
+  set {
+    name = "sqsQueueUrl"
+    value = aws_sqs_queue.sqs_scanning.url
+  }
+
+  set {
+    name = "codeBuildProject"
+    value = module.codebuild.project_name
+  }
+}
