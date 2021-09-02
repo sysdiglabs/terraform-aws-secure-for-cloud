7d142829a701ce78f13691a4af4be373625e7ee2
iru <irune.prado@sysdig.com>
2021-09-02 10:32:22 +0200

    chore(doc): diagram review

--- examples/organizational/diagram-org.py
+++ examples/organizational/diagram-org.py
@@ -1,6 +1,6 @@
 # diagrams as code vía https://diagrams.mingrammer.com
 from diagrams import Cluster, Diagram, Edge, Node
-from diagrams.aws.compute import ElasticContainerServiceService
+from diagrams.aws.compute import ElasticContainerServiceService, ECR
 from diagrams.aws.devtools import Codebuild
 from diagrams.aws.general import General
 from diagrams.aws.integration import SNS, SQS
@@ -14,68 +14,83 @@ diagram_attr = {
 }
 
 role_attr = {
-   "height":"1",
-   "width":"0.8",
+   "imagescale":"false",
+   "height":"1.5",
+   "width":"3",
    "fontsize":"9",
 }
 
-event_color="firebrick"
+color_event="firebrick"
+color_scanning = "dark-green"
+color_permission="red"
+color_non_important="gray"
+color_sysdig="lightblue"
 
-with Diagram("Sysdig Secure for Cloud\n(organizational usecase)", graph_attr=diagram_attr, filename="diagram-org", show=True):
+
+
+with Diagram("Sysdig Secure for Cloud\n(organizational)", graph_attr=diagram_attr, filename="diagram-org", show=True, direction="LR"):
 
     with Cluster("AWS organization"):
 
-        with Cluster("member accounts (main targets)", graph_attr={"bgcolor":"lightblue"}):
-            member_accounts = [General("account-1"), General("account-2"), General("..."), General("account-n")]
 
-            org_member_role = IAMRole("OrganizationAccountAccessRole\n(created by AWS for org. member accounts)", **role_attr)
-
-
-        with Cluster("master account"):
-
+        with Cluster("management account"):
 
             cloudtrail              = Cloudtrail("cloudtrail", shape="plaintext")
-            cloudtrail_legend       = ("for clarity purpose events received from 'secure for cloud' member account\n\
-                                    and master account have been removed from diagram, but will be processed too ")
 
-            Node(label=cloudtrail_legend, width="5",shape="plaintext", labelloc="t", fontsize="10")
 
-            master_credentials      = IAM("credentials \npermissions: cloudtrail, role creation,...", fontsize="10")
-            secure_for_cloud_role   = IAMRole("SysdigSecureForCloudRole", **role_attr)
+            management_credentials  = IAM("credentials \npermissions: cloudtrail, role creation,...", fontsize="10")
+            secure_for_cloud_role   = IAMRole("SysdigSecureForCloudRole\n\(enabled to assumeRole on `OrganizationAccountAccessRole`)", **role_attr)
             cloudtrail_s3           = S3("cloudtrail-s3-events")
             sns                     = SNS("cloudtrail-sns-events", comment="i'm a graph")
 
-            cloudtrail >> Edge(color=event_color, style="dashed") >> cloudtrail_s3 >> Edge(color=event_color, style="dashed") >> sns
+            cloudtrail >> Edge(color=color_event, style="dashed") >> cloudtrail_s3 >> Edge(color=color_event, style="dashed") >> sns
 
+        with Cluster("member accounts (main targets)", graph_attr={"bgcolor":"lightblue"}):
+            member_accounts = General("account-1..n")
+            org_member_role_1 = IAMRole("OrganizationAccountAccessRole\n(created by AWS for org. member accounts)", **role_attr)
+            ecr = ECR("container-registry\n *within any account")
 
 
         with Cluster("member account (secure for cloud)", graph_attr={"bgcolor":"seashell2"}):
 
-            org_member_role = IAMRole("OrganizationAccountAccessRole\n(created by AWS for org. member accounts)", **role_attr)
+            org_member_role_2 = IAMRole("OrganizationAccountAccessRole\n(created by AWS for org. member accounts)", **role_attr)
+
+            sqs         = SQS("cloudtrail-sqs")
+            s3_config   = S3("cloud-connector-config")
+            cloudwatch  = Cloudwatch("cloudwatch\nlogs and alarms")
+            codebuild   = Codebuild("codebuild project")
 
             with Cluster("ecs-cluster"):
                 cloud_connector = ElasticContainerServiceService("cloud-connector")
                 cloud_scanning = ElasticContainerServiceService("cloud-scanning")
 
-            sqs         = SQS("cloudtrail-sqs")
-            s3_config   = S3("cloud-connector-config")
-            cloudwatch  = Cloudwatch("cloudwatch\nlogs and alarms")
-            codebuild = Codebuild("codebuild project")
-
-            sqs << Edge(color=event_color) << cloud_connector
-            sqs << Edge(color=event_color) << cloud_scanning
-            cloud_connector - s3_config
-            cloud_connector >> cloudwatch
+            sqs << Edge(color=color_event) << cloud_connector
+            sqs << Edge(color=color_event) << cloud_scanning
+            cloud_connector - Edge(color=color_non_important) - s3_config
+            cloud_connector >> Edge(color=color_non_important) >> cloudwatch
+            cloud_scanning  >>  Edge(color=color_non_important) >> cloudwatch
             cloud_scanning >> codebuild
+            codebuild >> Edge(color=color_non_important) >>  ecr
 
 
-        member_accounts >> Edge(color=event_color, style="dashed") >>  cloudtrail
-        sns >> Edge(color=event_color, style="dashed") >> sqs
-#        cloudtrail_s3 << Edge(color=event_color) << cloud_connector
-        (cloudtrail_s3 << Edge(color=event_color) << secure_for_cloud_role) -  Edge(xlabel="assumeRole", color=event_color) - cloud_connector
+        member_accounts >> Edge(color=color_event, style="dashed") >>  cloudtrail
+        sns >> Edge(color=color_event, style="dashed") >> sqs
+#        cloudtrail_s3 << Edge(color=color_non_important) << cloud_connector
+#        cloudtrail_s3 << Edge(color=color_non_important) << cloud_scanning
+#        secure_for_cloud_role <<  Edge(color=color_permission, fontcolor=color_permission, xlabel="assumeRole") << cloud_connector
+#        (cloudtrail_s3 << Edge(color=color_event) <<
+
+
 
     with Cluster("AWS account (sysdig)"):
         sds = Custom("Sysdig Secure", "../../resources/diag-sysdig-icon.png")
 
-    cloud_connector >> sds
-    codebuild >> sds
+
+    cloud_connector >> Edge(color=color_sysdig) >> sds
+    codebuild >> Edge(color=color_sysdig) >>  sds
+
+#    secure_for_cloud_role >> Edge(color=color_permission, fontcolor=color_permission, xlable="assumeRole") >>  org_member_role_1
+
+
+#    cloudtrail_legend  = ("to simplify,\l- events received from 'secure for cloud' member account  and management account have been removed from diagram, but will be processed too")
+#    Node(label=cloudtrail_legend, shape="plaintext", labelloc="t", width="10", fontsize="10" )
