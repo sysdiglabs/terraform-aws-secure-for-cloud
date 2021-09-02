9644519e724fa70343c8bf8d6025ed858c1ad053
iru <irune.prado@sysdig.com>
2021-09-02 19:55:54 +0200

    fix(typo)

--- examples/single-account/main.tf
+++ examples/single-account/main.tf
@@ -103,10 +103,10 @@ module "cloud_scanning" {
 data "aws_caller_identity" "me" {}
 
 provider "sysdig" {
-  sysdig_secure_url             = var.sysdig_secure_endpoint
-  sysdig_secure_api_token       = var.sysdig_secure_api_token
-  sysdig_secure_insecure_tls    = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false : true
-} 
+  sysdig_secure_url          = var.sysdig_secure_endpoint
+  sysdig_secure_api_token    = var.sysdig_secure_api_token
+  sysdig_secure_insecure_tls = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false : true
+}
 
 module "cloud_bench" {
   source = "../../modules/services/cloud-bench"
