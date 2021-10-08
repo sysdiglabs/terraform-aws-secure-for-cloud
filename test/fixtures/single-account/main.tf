7a4f37623fab3c9bfcee83bccdd53dfb7ccd2d2d
iru <irune.prado@sysdig.com>
2021-10-08 13:36:02 +0200

    chore(tests): enable benchmarks to ci/cd tests

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -1,6 +1,5 @@
-provider "aws" {}
 module "cloudvision_aws_single_account" {
-  source = "../../../examples-internal/single-account-without-bench"
+  source = "../../../examples/single-account"
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
