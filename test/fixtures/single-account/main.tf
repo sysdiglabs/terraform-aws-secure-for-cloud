0b8f9674bd706cdb0b153d6bcaf4ca88524acd55
iru <irune.prado@sysdig.com>
2021-10-08 13:59:18 +0200

    chore(tests): 409 conflict testing

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -3,6 +3,6 @@ module "cloudvision_aws_single_account" {
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
-  name                    = var.name
-  region                  = "quicktest-${var.region}"
+  name                    = "quicktest-${var.name}"
+  region                  = var.region
 }
