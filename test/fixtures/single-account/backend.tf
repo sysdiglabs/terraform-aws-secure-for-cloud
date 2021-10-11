62c5cee550363c5ea679bd0f9c4ef3e426c6722a
iru <irune.prado@sysdig.com>
2021-10-11 15:58:32 +0200

    chore(tests): enable remote state

--- test/fixtures/single-account/backend.tf
+++ test/fixtures/single-account/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-cicd-tests"
-    key            = "single-account-terraform.tfstate"
+    key            = "single-account/terraform.tfstate"
     dynamodb_table = "terraform-cicd-test"
     region         = "eu-west-3"
   }
