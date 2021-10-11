97e0c663c647e367f712f660c8fcb9357f7cb9e4
iru <irune.prado@sysdig.com>
2021-10-11 20:12:26 +0200

    chore(tests): add organizational test

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -3,6 +3,6 @@ module "cloudvision_aws_single_account" {
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
-  name                    = "quicktest-${var.name}"
+  name                    = var.name
   region                  = var.region
 }
