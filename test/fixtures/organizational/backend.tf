efd7dcc863e95c185f1a50facf328f185778eb32
iru <irune.prado@sysdig.com>
2021-10-14 20:48:43 +0200

    chore(test): fix backend with profile usage
    cannot make it work with multiple profile usage on organization :(

--- test/fixtures/organizational/backend.tf
+++ test/fixtures/organizational/backend.tf
@@ -1,8 +1,7 @@
 # Terraform state storage backend
 terraform {
   backend "s3" {
-    profile        = "aws_cloudnative"
-    bucket         = "secure-cloud-terraform-tests"
+    bucket         = "secure-cloud-terraform-tests-org" # need to append '-org' to avoid conflict
     key            = "aws-organizational/terraform.tfstate"
     dynamodb_table = "secure-cloud-terraform-tests"
     region         = "eu-west-3"
