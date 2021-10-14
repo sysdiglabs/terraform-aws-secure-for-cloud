8abeb140039dadaa32868b4ffd752b7dad42048b
iru <irune.prado@sysdig.com>
2021-10-14 18:54:37 +0200

    chore(test): fix backend with profile usage

--- test/fixtures/organizational/backend.tf
+++ test/fixtures/organizational/backend.tf
@@ -1,8 +1,7 @@
 # Terraform state storage backend
 terraform {
   backend "s3" {
-    access_key     = var.backend_aws_access_key
-    secret_key     = var.backend_aws_secret_access_key
+    profile        = "aws_clodnative"
     bucket         = "terraform-cicd-tests"
     key            = "aws-organizational/terraform.tfstate"
     dynamodb_table = "terraform-cicd-test"
