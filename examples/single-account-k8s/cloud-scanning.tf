6e3b16ad2a002d758902b446b4a7ca943c3fc120
iru <irune.prado@sysdig.com>
2021-10-19 10:49:13 +0200

    chore: naming adjustments

--- examples/single-account-k8s/cloud-scanning.tf
+++ examples/single-account-k8s/cloud-scanning.tf
@@ -3,7 +3,7 @@
 #-------------------------------------
 module "cloud_scanning_sqs" {
   source        = "../../modules/infrastructure/cloudtrail-subscription-sqs"
-  name          = "${var.name}-cloud_scanning"
+  name          = var.name
   sns_topic_arn = module.cloudtrail.sns_topic_arn
   tags          = var.tags
 }
