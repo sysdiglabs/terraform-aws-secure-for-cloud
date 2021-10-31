553eb94f32211a9bf6f8e230a0179106af05809a
iru <irune.prado@sysdig.com>
2021-10-31 17:06:04 +0100

    chore(refact): homogeneize through eamples

--- examples/single-account-k8s/main.tf
+++ examples/single-account-k8s/main.tf
@@ -25,14 +25,3 @@ module "ssm" {
   name                    = var.name
   sysdig_secure_api_token = var.sysdig_secure_api_token
 }
-
-module "cloudtrail" {
-  source = "../../modules/infrastructure/cloudtrail"
-  name   = var.name
-
-  is_organizational     = false
-  is_multi_region_trail = var.cloudtrail_is_multi_region_trail
-  cloudtrail_kms_enable = var.cloudtrail_kms_enable
-
-  tags = var.tags
-}
