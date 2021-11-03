28564ba7b4d2be3dc94acea4360a47ab0b50cfd1
iru <irune.prado@sysdig.com>
2021-11-03 13:14:40 +0100

    chore(utils): add utils

--- examples-internal/utils-cloudtrail_s3-sns-sqs/variables.tf
+++ examples-internal/utils-cloudtrail_s3-sns-sqs/variables.tf
@@ -2,12 +2,6 @@ variable "cloudtrail_s3_name" {
   type = string
 }
 
-
-#variable "sysdig_secure_for_cloud_member_account_id" {
-#  type        = string
-#  description = "Permission provisioning. Organizational member account where Sysdig Secure for Cloud workload will be deployed."
-#}
-#
 #---------------------------------
 # optionals - with defaults
 #---------------------------------
@@ -16,30 +10,6 @@ variable "event_notification_path_filter" {
   type    = string
   default = "*"
 }
-
-#variable "organizational_member_default_admin_role" {
-#  type        = string
-#  default     = "OrganizationAccountAccessRole"
-#  description = "Default role created by AWS for managed-account users to be able to admin member accounts.<br/>https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_accounts_access.html"
-#}
-#
-##
-## cloudtrail configuration
-##
-#
-#variable "cloudtrail_is_multi_region_trail" {
-#  type        = bool
-#  default     = true
-#  description = "true/false whether cloudtrail will ingest multiregional events. testing/economization purpose."
-#}
-#
-#variable "cloudtrail_kms_enable" {
-#  type        = bool
-#  default     = true
-#  description = "true/false whether cloudtrail delivered events to S3 should persist encrypted"
-#}
-#
-
 
 ##
 ## general
