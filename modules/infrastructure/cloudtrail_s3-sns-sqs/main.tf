289ba9d520166c6ad2f2f467c59cd70aec4f3114
iru <irune.prado@sysdig.com>
2021-11-03 15:48:25 +0100

    chore(refact): naming

--- modules/infrastructure/cloudtrail_s3-sns-sqs/main.tf
+++ modules/infrastructure/cloudtrail_s3-sns-sqs/main.tf
@@ -44,6 +44,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = data.aws_s3_bucket.cloudtrail_s3.id
 
   topic {
+    id            = "${var.name}-notif"
     topic_arn     = aws_sns_topic.s3_sns.arn
     events        = ["s3:ObjectCreated:*"]
     filter_prefix = var.event_notification_path_filter
