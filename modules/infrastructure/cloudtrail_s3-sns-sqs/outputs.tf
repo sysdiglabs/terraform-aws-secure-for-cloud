c7de5573f02d6684c20039521b0bbfb5ffed9ea7
iru <irune.prado@sysdig.com>
2021-11-03 17:11:05 +0100

    chore(utils): add readme and outputs

--- modules/infrastructure/cloudtrail_s3-sns-sqs/outputs.tf
+++ modules/infrastructure/cloudtrail_s3-sns-sqs/outputs.tf
@@ -1,3 +1,8 @@
+output "cloudtrail_subscribed_sqs_url" {
+  value       = module.cloudtrail_s3_sns_sqs.cloudtrail_sns_subscribed_sqs_url
+  description = "URL of the SQS topic subscribed to the SNS of Cloudtrail-S3 bucket"
+}
+
 output "cloudtrail_subscribed_sqs_arn" {
   value       = module.cloudtrail_s3_sns_sqs.cloudtrail_sns_subscribed_sqs_arn
   description = "ARN of the SQS topic subscribed to the SNS of Cloudtrail-S3 bucket"
