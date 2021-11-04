a74ff7810611b26e8101d9d5842f3fa3fa497bef
iru <irune.prado@sysdig.com>
2021-11-04 10:25:14 +0100

    chore(refact): remove unused role
    [skip ci]

--- modules/infrastructure/permissions/eks-org-role/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sysdig_secure_for_cloud_role_arn" {
-  value       = aws_iam_role.secure_for_cloud_role.arn
-  description = "organizational secure-for-cloud role arn"
-}
-
-output "sysdig_secure_for_cloud_role_name" {
-  value       = aws_iam_role.secure_for_cloud_role.name
-  description = "organizational secure-for-cloud role name"
-}
