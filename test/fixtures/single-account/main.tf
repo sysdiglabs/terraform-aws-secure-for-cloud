ae0862ddcb3e28d3210e6ef55c016d2c68b5ed50
iru <irune.prado@sysdig.com>
2021-11-10 10:33:15 +0100

    chore(test): lint fix

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -8,5 +8,4 @@ module "cloudvision_aws_single_account" {
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
-  region                  = var.region
 }
