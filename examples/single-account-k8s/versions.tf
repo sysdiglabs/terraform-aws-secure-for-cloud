275a170223e9a8f4a4fb7ca48e27de2ada559608
Alex <alex.qiu@sysdig.com>
2022-02-07 10:23:15 -0800

    update required sysdig provider

--- examples/single-account-k8s/versions.tf
+++ examples/single-account-k8s/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = ">= 0.5.19"
+      version = ">= 0.5.29"
     }
     helm = {
       source  = "hashicorp/helm"
