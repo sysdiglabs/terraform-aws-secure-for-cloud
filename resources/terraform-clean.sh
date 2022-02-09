74204be96c7ccb3c6e2bd8789b04bfa4a510b039
iru <irune.prado@sysdig.com>
2022-02-09 10:29:05 +0100

    chore: fix. cleaner should not touch test

--- resources/terraform-clean.sh
+++ resources/terraform-clean.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd ..
+# we don't wanna delete possible ./test state tests
+
+cd ../examples
+find . -name ".terraform" -exec rm -fr {} \;
+find . -name "terraform.tfstate*" -exec rm -fr {} \;
+find . -name ".terraform.lock.hcl*" -exec rm -fr {} \;
+
+cd ../modules
 find . -name ".terraform" -exec rm -fr {} \;
 find . -name "terraform.tfstate*" -exec rm -fr {} \;
 find . -name ".terraform.lock.hcl*" -exec rm -fr {} \;
