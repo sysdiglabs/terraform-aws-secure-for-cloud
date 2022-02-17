26be6adba42b0695af8a83b4211410a0a711eddb
hayk99 <hayk.kocharyan@sysdig.com>
2022-02-17 13:11:45 +0100

    chore: remove auto-assign ipv4

--- modules/infrastructure/ecs-vpc/vpc.tf
+++ modules/infrastructure/ecs-vpc/vpc.tf
@@ -10,6 +10,7 @@ module "vpc" {
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  map_public_ip_on_launch = false
 
   azs = length(var.ecs_vpc_region_azs) > 0 ? var.ecs_vpc_region_azs : [data.aws_availability_zones.zones.names[0], data.aws_availability_zones.zones.names[1]]
 
