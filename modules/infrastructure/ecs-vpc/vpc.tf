21e7bd3dae437319b34130ea0c2c15f9f72fe6b3
hayk99 <hayk.kocharyan@sysdig.com>
2022-02-17 13:16:50 +0100

    fix: fix precommit

--- modules/infrastructure/ecs-vpc/vpc.tf
+++ modules/infrastructure/ecs-vpc/vpc.tf
@@ -8,11 +8,14 @@ module "vpc" {
   name = "${var.name}-vpc"
   cidr = "10.0.0.0/16"
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
   map_public_ip_on_launch = false
 
-  azs = length(var.ecs_vpc_region_azs) > 0 ? var.ecs_vpc_region_azs : [data.aws_availability_zones.zones.names[0], data.aws_availability_zones.zones.names[1]]
+  azs = length(var.ecs_vpc_region_azs) > 0 ? var.ecs_vpc_region_azs : [
+    data.aws_availability_zones.zones.names[0],
+    data.aws_availability_zones.zones.names[1]
+  ]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
