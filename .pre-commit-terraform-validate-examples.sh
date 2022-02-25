bc6234d7e7f186194402daf24b8ad30559dcc7ea
iru <irune.prado@sysdig.com>
2022-02-25 18:33:20 +0100

    ci: test validate

--- .pre-commit-terraform-validate-examples.sh
+++ .pre-commit-terraform-validate-examples.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # ensure errexit + failfast
-set -o errexit
+#set -o errexit
 
 # cleanup
+echo "cleaning .terraform state"
 bash ./resources/terraform-clean.sh
 
-for dir in examples*/*
+for dir in $(find . -name 'versions.tf' -not -path '*.terraform*' -exec dirname {} \;)
 do
-
+  echo validating [$dir]
   # skip aliased providers due to terraform validate unresolved bug
   # https://github.com/hashicorp/terraform/issues/28490
-  if [ $dir == "examples/organizational" ]; then
+  if [ "$dir" == "examples/organizational" ]; then
     echo "skipping validation on [$dir]"
     break
   fi
-  echo validating example [$dir]
-  cd $dir
-  terraform init
+  pushd .
+  cd "$dir"
+  # force init
+  # https://github.com/antonbabenko/pre-commit-terraform/issues/224
+  terraform init --upgrade
   terraform validate
-  cd ../..
+  popd
 done
