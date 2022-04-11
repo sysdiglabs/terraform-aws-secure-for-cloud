7d1c07c25b3d90bce82265e70342e017c0f62354
hayk99 <hayk.kocharyan@sysdig.com>
2022-04-11 12:38:23 +0200

    fix: remove index

--- examples/single-account-k8s/credentials.tf
+++ examples/single-account-k8s/credentials.tf
@@ -6,6 +6,6 @@ module "iam_user" {
 
   ssm_secure_api_token_arn       = module.ssm.secure_api_token_secret_arn
   cloudtrail_s3_bucket_arn       = length(module.cloudtrail) > 0 ? module.cloudtrail[0].s3_bucket_arn : "*"
-  cloudtrail_subscribed_sqs_arn  = module.cloud_connector_sqs[0].cloudtrail_sns_subscribed_sqs_arn
+  cloudtrail_subscribed_sqs_arn  = module.cloud_connector_sqs.cloudtrail_sns_subscribed_sqs_arn
   scanning_codebuild_project_arn = length(module.codebuild) > 0 ? module.codebuild[0].project_arn : "*"
 }
