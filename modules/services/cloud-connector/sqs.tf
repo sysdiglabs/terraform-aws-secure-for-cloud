4558947cc603b8b0683492d66d2e29209a5c36dc
hayk99 <hayk.kocharyan@sysdig.com>
2022-04-13 10:06:10 +0200

    ci: trigger precommit with MD files

--- modules/services/cloud-connector/sqs.tf
+++ modules/services/cloud-connector/sqs.tf
@@ -1,6 +1,5 @@
 module "cloud_connector_sqs" {
   source        = "../../infrastructure/sqs-sns-subscription"
-  name          = "${var.name}-cloud_connector"
   sns_topic_arn = var.sns_topic_arn
   tags          = var.tags
 }
