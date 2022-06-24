a5b26f20b0c48d860af608ebdf17ec0b03996256
pawan <pawnu@users.noreply.github.com>
2022-06-24 20:50:49 +0100

    Output org role if it is inherited

--- modules/services/cloud-connector-ecs/outputs.tf
+++ modules/services/cloud-connector-ecs/outputs.tf
@@ -1,4 +1,4 @@
 output "ecs_task_role_arn" {
-    value = aws_iam_role.task.arn
+    value = var.is_organizational ? data.aws_iam_role.task_inherited[0].arn : aws_iam_role.task[0].arn
     description = "ECS task role with permissions to CloudTrail s3 logs and SNS notifications through SQS."
 }
