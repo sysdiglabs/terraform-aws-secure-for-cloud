d98f7080bf03305d801dfd7b12c9963f0a72d9ef
hayk99 <hayk.kocharyan@sysdig.com>
2022-06-28 12:37:32 +0200

    Revert "feat: ecs cloud connector based on binary scanner"
    This reverts commit 6d37ea00701e73d4deb93642477c03c69c1fe890.

--- examples/single-account-ecs/main.tf
+++ examples/single-account-ecs/main.tf
@@ -1,8 +1,3 @@
-locals {
-  deploy_image_scanning   = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs
-  deploy_scanning_infra   = local.deploy_image_scanning && !var.use_standalone_scanner
-
-}
 #-------------------------------------
 # general resources
 #-------------------------------------
@@ -26,13 +21,13 @@ module "ssm" {
 #
 
 module "codebuild" {
-  count = local.deploy_scanning_infra ? 1 : 0
+  count = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs ? 1 : 0
 
   source                       = "../../modules/infrastructure/codebuild"
   name                         = "${var.name}-codebuild"
   secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
 
-  tags       = var.tags
+  tags = var.tags
   # note. this is required to avoid racing conditions
   depends_on = [module.ssm]
 }
@@ -50,7 +45,6 @@ module "cloud_connector" {
 
   deploy_image_scanning_ecr = var.deploy_image_scanning_ecr
   deploy_image_scanning_ecs = var.deploy_image_scanning_ecs
-  use_standalone_scanner    = var.use_standalone_scanner
 
   is_organizational = false
 
