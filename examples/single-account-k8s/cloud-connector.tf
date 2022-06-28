4b98cb217b17755c8d32c88828bdde1f65b1274f
hayk99 <hayk.kocharyan@sysdig.com>
2022-06-28 12:37:40 +0200

    Revert "feat: create k8s config base on binary scanner use"
    This reverts commit 06318906df01887b1e8467eb4db6b30539c77449.

--- examples/single-account-k8s/cloud-connector.tf
+++ examples/single-account-k8s/cloud-connector.tf
@@ -1,10 +1,5 @@
 locals {
-  deploy_image_scanning   = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs
-  deploy_scanning_infra   = local.deploy_image_scanning && !var.use_standalone_scanner
-  ecr_standalone_scanning = var.deploy_image_scanning_ecr && var.use_standalone_scanner
-  ecs_standalone_scanning = var.deploy_image_scanning_ecs && var.use_standalone_scanner
-  ecr_scanning_with_infra = var.deploy_image_scanning_ecr && !var.use_standalone_scanner
-  ecs_scanning_with_infra = var.deploy_image_scanning_ecs && !var.use_standalone_scanner
+  deploy_image_scanning = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs
 }
 
 #-------------------------------------
@@ -19,13 +14,13 @@ module "cloud_connector_sqs" {
 }
 
 module "codebuild" {
-  count  = local.deploy_scanning_infra ? 1 : 0
+  count  = local.deploy_image_scanning ? 1 : 0
   source = "../../modules/infrastructure/codebuild"
 
   name                         = var.name
   secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
 
-  tags       = var.tags
+  tags = var.tags
   # note. this is required to avoid race conditions
   depends_on = [module.ssm]
 }
@@ -71,10 +66,8 @@ resource "helm_release" "cloud_connector" {
     value = "terraform_aws_k8s_single"
   }
 
-  values     = [
+  values = [
     yamlencode({
-      logging   = "info"
-      rules     = []
       ingestors = [
         {
           cloudtrail-sns-sqs = {
@@ -82,28 +75,20 @@ resource "helm_release" "cloud_connector" {
           }
         }
       ]
-      scanners  = [
-        merge(
-        local.ecr_scanning_with_infra ? {
+      scanners = local.deploy_image_scanning ? [
+        merge(var.deploy_image_scanning_ecr ? {
           aws-ecr = {
             codeBuildProject         = module.codebuild[0].project_name
             secureAPITokenSecretName = module.ssm.secure_api_token_secret_name
           }
-        } : {},
-        local.ecs_scanning_with_infra ? {
-          aws-ecs = {
-            codeBuildProject         = module.codebuild[0].project_name
-            secureAPITokenSecretName = module.ssm.secure_api_token_secret_name
-          }
-        } : {},
-        local.ecr_standalone_scanning ? {
-          aws-ecr-inline = {},
-        } : {},
-        local.ecs_standalone_scanning ? {
-          aws-ecs-inline = {}
-        } : {},
-        )
-      ]
+          } : {},
+          var.deploy_image_scanning_ecs ? {
+            aws-ecs = {
+              codeBuildProject         = module.codebuild[0].project_name
+              secureAPITokenSecretName = module.ssm.secure_api_token_secret_name
+            }
+        } : {})
+      ] : []
     })
   ]
   depends_on = [module.iam_user]
