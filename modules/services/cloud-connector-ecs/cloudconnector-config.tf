a9bc239f66d786576a88f14135bdea9fe0a74bcc
hayk99 <hayk.kocharyan@sysdig.com>
2022-06-28 12:36:08 +0200

    Revert "chore: fix precommit errors"
    This reverts commit 97c7c2c164205ed705cc4e4e4eea9d4ba78f6a45.

--- modules/services/cloud-connector-ecs/cloudconnector-config.tf
+++ modules/services/cloud-connector-ecs/cloudconnector-config.tf
@@ -1,50 +1,50 @@
 locals {
   default_config = yamlencode(merge({
-    logging = "info"
-    rules   = []
+    logging   = "info"
+    rules     = []
     ingestors = [
       {
         cloudtrail-sns-sqs = merge(
-          {
-            queueURL = module.cloud_connector_sqs.cloudtrail_sns_subscribed_sqs_url
-          },
-          var.is_organizational ? {
-            assumeRole = var.organizational_config.sysdig_secure_for_cloud_role_arn
-          } : {}
+        {
+          queueURL = module.cloud_connector_sqs.cloudtrail_sns_subscribed_sqs_url
+        },
+        var.is_organizational ? {
+          assumeRole = var.organizational_config.sysdig_secure_for_cloud_role_arn
+        } : {}
         )
       }
     ]
-    },
-    {
-      scanners = local.deploy_image_scanning ? [
-        merge(
-          local.ecs_scanning_with_infra ? {
-            aws-ecr = merge({
-              codeBuildProject         = var.build_project_name
-              secureAPITokenSecretName = var.secure_api_token_secret_name
-              },
-              var.is_organizational ? {
-                masterOrganizationRole       = var.organizational_config.sysdig_secure_for_cloud_role_arn
-                organizationalRolePerAccount = var.organizational_config.organizational_role_per_account
-            } : {})
-          } : {},
-          local.ecs_scanning_with_infra ? {
-            aws-ecs = merge({
-              codeBuildProject         = var.build_project_name
-              secureAPITokenSecretName = var.secure_api_token_secret_name
-              },
-              var.is_organizational ? {
-                masterOrganizationRole       = var.organizational_config.sysdig_secure_for_cloud_role_arn
-                organizationalRolePerAccount = var.organizational_config.organizational_role_per_account
-            } : {})
-        } : {}),
-        local.ecr_standalone_scanning ? {
-          aws-ecr-inline = {},
-        } : {},
-        local.ecs_standalone_scanning ? {
-          aws-ecs-inline = {},
-        } : {}
-      ] : []
-    }
+  },
+  {
+    scanners = local.deploy_image_scanning ? [
+      merge(
+      local.ecs_scanning_with_infra ? {
+        aws-ecr = merge({
+          codeBuildProject         = var.build_project_name
+          secureAPITokenSecretName = var.secure_api_token_secret_name
+        },
+        var.is_organizational ? {
+          masterOrganizationRole       = var.organizational_config.sysdig_secure_for_cloud_role_arn
+          organizationalRolePerAccount = var.organizational_config.organizational_role_per_account
+        } : {})
+      } : {},
+      local.ecs_scanning_with_infra ? {
+        aws-ecs = merge({
+          codeBuildProject         = var.build_project_name
+          secureAPITokenSecretName = var.secure_api_token_secret_name
+        },
+        var.is_organizational ? {
+          masterOrganizationRole       = var.organizational_config.sysdig_secure_for_cloud_role_arn
+          organizationalRolePerAccount = var.organizational_config.organizational_role_per_account
+        } : {})
+      } : {}),
+      local.ecr_standalone_scanning ? {
+        aws-ecr-inline = {},
+      } : {},
+      local.ecs_standalone_scanning ? {
+        aws-ecs-inline = {},
+      } : {}
+    ] : []
+  }
   ))
 }
