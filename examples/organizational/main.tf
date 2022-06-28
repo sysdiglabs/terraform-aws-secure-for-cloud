48637c915ba727f917d8ede2b312981dc9e8d3d5
hayk99 <hayk.kocharyan@sysdig.com>
2022-06-28 12:37:27 +0200

    Revert "feat: org cloud connector based on binary scanner"
    This reverts commit 26d85ded661dcad6b462bce9bb908f375b9753c5.

--- examples/organizational/main.tf
+++ examples/organizational/main.tf
@@ -1,7 +1,3 @@
-locals {
-  deploy_image_scanning   = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs
-  deploy_scanning_infra   = local.deploy_image_scanning && !var.use_standalone_scanner
-}
 #-------------------------------------
 # resources deployed always in management account
 # with default provider
@@ -17,16 +13,16 @@ module "resource_group_secure_for_cloud_member" {
   providers = {
     aws = aws.member
   }
-  source    = "../../modules/infrastructure/resource-group"
-  name      = var.name
-  tags      = var.tags
+  source = "../../modules/infrastructure/resource-group"
+  name   = var.name
+  tags   = var.tags
 }
 
 #-------------------------------------
 # secure-for-cloud member account workload
 #-------------------------------------
 module "ssm" {
-  providers               = {
+  providers = {
     aws = aws.member
   }
   source                  = "../../modules/infrastructure/ssm"
@@ -40,16 +36,16 @@ module "ssm" {
 # cloud-connector
 #-------------------------------------
 module "codebuild" {
-  count = local.deploy_scanning_infra ? 1 : 0
+  count = var.deploy_image_scanning_ecr || var.deploy_image_scanning_ecs ? 1 : 0
 
-  providers                    = {
+  providers = {
     aws = aws.member
   }
   source                       = "../../modules/infrastructure/codebuild"
   name                         = var.name
   secure_api_token_secret_name = module.ssm.secure_api_token_secret_name
 
-  tags       = var.tags
+  tags = var.tags
   # note. this is required to avoid race conditions
   depends_on = [module.ssm]
 }
@@ -66,9 +62,8 @@ module "cloud_connector" {
 
   deploy_image_scanning_ecr = var.deploy_image_scanning_ecr
   deploy_image_scanning_ecs = var.deploy_image_scanning_ecs
-  use_standalone_scanner    = var.use_standalone_scanner
 
-  is_organizational     = true
+  is_organizational = true
   organizational_config = {
     sysdig_secure_for_cloud_role_arn = module.secure_for_cloud_role.sysdig_secure_for_cloud_role_arn
     organizational_role_per_account  = var.organizational_member_default_admin_role
