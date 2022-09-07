4f1f1bf54d0b2b25a95898658ddea07af9508615
iru <irune.prado@sysdig.com>
2022-09-07 17:02:21 +0200

    chore: test reorg

--- test/fixtures/organizational-k8s/backend.tf
+++ test/fixtures/organizational-k8s/backend.tf
@@ -1,7 +1,7 @@
 # Terraform state storage backend
 terraform {
   backend "s3" {
-    bucket         = "secure-cloud-terraform-tests" # need to append '-org' to avoid conflict?
+    bucket         = "secure-cloud-terraform-tests"
     key            = "aws-organizational-k8s-reuse_cloudtrail/terraform.tfstate"
     dynamodb_table = "secure-cloud-terraform-tests"
     region         = "eu-west-3"
