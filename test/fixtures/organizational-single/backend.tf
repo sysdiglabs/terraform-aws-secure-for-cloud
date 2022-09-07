cf0e9dbe86d38a43a470db34b855bd8e94591cd8
iru <irune.prado@sysdig.com>
2022-09-07 17:19:14 +0200

    chore: fix org state bucket -_-

--- test/fixtures/organizational-single/backend.tf
+++ test/fixtures/organizational-single/backend.tf
@@ -1,7 +1,7 @@
 # Terraform state storage backend
 terraform {
   backend "s3" {
-    bucket         = "secure-cloud-terraform-tests"
+    bucket         = "secure-cloud-terraform-tests-org" # org examples deploy in qa org/s3 bucket
     key            = "aws-organizational-single/terraform.tfstate"
     dynamodb_table = "secure-cloud-terraform-tests"
     region         = "eu-west-3"
--- test/fixtures/organizational/backend.tf
+++ test/fixtures/organizational/backend.tf
@@ -1,7 +1,7 @@
 # Terraform state storage backend
 terraform {
   backend "s3" {
-    bucket         = "secure-cloud-terraform-tests"
+    bucket         = "secure-cloud-terraform-tests-org" # org examples deploy in org/s3 bucket
     key            = "aws-organizational/terraform.tfstate"
     dynamodb_table = "secure-cloud-terraform-tests"
     region         = "eu-west-3"
