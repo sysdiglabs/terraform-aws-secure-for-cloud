acfb099a3533d5f42ebd3c22f147bea929d168ac
Miguel Angel Baztan <miguel.baztan@sysdig.com>
2022-09-21 13:17:25 +0200

    fix: wire beta_image_ecr and deploy it

--- modules/services/cloud-connector-ecs/locals.tf
+++ modules/services/cloud-connector-ecs/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  deploy_image_scanning = var.deploy_image_scanning_ecs || var.deploy_image_scanning_ecr
+  deploy_image_scanning = var.deploy_image_scanning_ecs || var.deploy_image_scanning_ecr || var.deploy_beta_image_scanning_ecr
 }
