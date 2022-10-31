9c4f86f26130288b016594e4b5393327c9999918
iru <irune.prado@sysdig.com>
2022-10-31 21:08:02 +0100

    docs: merge manual use-case for org-three-way with sns (#139)

---
+++ use-cases/utils/sysdig_cloud_compliance_check.sh
@@ -0,0 +1,4 @@
+# TODO
+# create an script to validate that after all provisioning (sysdig secure backend and AWS) compliance role works as expected
+
+https://secure.sysdig.com/swagger.html#tag/Cloud/paths/~1api~1cloud~1v2~1accounts~1{accountId}~1validateRole/get
