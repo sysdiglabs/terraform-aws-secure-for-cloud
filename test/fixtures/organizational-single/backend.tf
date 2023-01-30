d7c778aa5638a9c00d293d9d4382b58c7f7d3bc4
iru <irune.prado@sysdig.com>
2023-01-30 10:51:11 +0100

    chore: deleted test, it does not make sense anymore

--- test/fixtures/organizational-single/backend.tf
+++
@@ -1,9 +0,0 @@
-# Terraform state storage backend
-terraform {
-  backend "s3" {
-    bucket         = "secure-cloud-terraform-tests-org" # org examples deploy in qa org/s3 bucket
-    key            = "aws-organizational-single/terraform.tfstate"
-    dynamodb_table = "secure-cloud-terraform-tests"
-    region         = "eu-west-3"
-  }
-}
