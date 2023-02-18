7663ed03148444a17c53f375370137cf591296ee
sameer-in <sameer.inamdar@sysdig.com>
2023-02-17 20:48:46 -0800

    adding failure tolerance

--- modules/services/cloud-bench/main.tf
+++ modules/services/cloud-bench/main.tf
@@ -153,4 +153,10 @@ resource "aws_cloudformation_stack_set_instance" "stackset_instance" {
   deployment_targets {
     organizational_unit_ids = [for root in data.aws_organizations_organization.org[0].roots : root.id]
   }
+  operation_preferences {
+    failure_tolerance_count = 100
+    # failure_tolerance_percentage = 100
+    max_concurrent_count = 2
+    # max_concurrent_percentage = 2
+  }
 }
